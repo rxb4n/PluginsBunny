f4eb5f6470aa72f617c328a9ff92f30e7edfe552
Jean Pierre Langhoff <125162733+BreadoWebTech@users.noreply.github.com>
2023-04-06 00:18:55 +0200

    Update index.js

--- plugins/greeting/src/index.js
+++ plugins/greeting/src/index.js
@@ -1,15 +1,55 @@
+import { registerCommand } from "@vendetta/commands";
+
+let helloCmd;
+
+export default {
+
+  onLoad: () => {
+
+    helloCmd = registerCommand({
+
+      name: "hello",
+
+      displayName: "Hello",
+
+      description: "Sends a greeting message",
+
+      displayDescription: "Sends a greeting message",
+
+      options: [],
+
+      execute: (args, context) => {
+
+        return { content: "Hello!" };
+
+      },
+
+      applicationId: -1,
+
+      inputType: 1,
+
+      type: 1,
+
+    });
+
+  },
+
+  onUnload: () => {
+
+    helloCmd();
+
+  },
+
+};
 
 
 
 
 
-{
-  name: "greeting",
-  description: "sends a greeting",
-  inputType: ApplicationCommandInputType.BUILT_IN_TEXT, // 1
-  execute(args, context) {
-    return { content: "whats good" }
-  }
-}
+
+
+
+
+
 
 
