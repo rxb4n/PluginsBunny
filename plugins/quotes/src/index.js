50853f46b6b3851f6bc11b7f5278277f3b91cd61
Jean Pierre Langhoff <125162733+BreadoWebTech@users.noreply.github.com>
2023-04-09 06:09:32 +0200

    Update index.js

--- plugins/quotes/src/index.js
+++ plugins/quotes/src/index.js
@@ -64,11 +64,11 @@ quotecommands.push(registerCommand({
 
   
 
-  applicationId: "-1",
+  //applicationId: "-1",
 
-  inputType: 1,
+  //inputType: 1,
 
-  type: 1,
+  //type: 1,
 
   execute: async (args, context) => {
 
