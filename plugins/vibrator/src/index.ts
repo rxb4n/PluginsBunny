a655b7d71583c7121ffa30559a75b0463215d2f1
Jean Pierre Langhoff <125162733+BreadoWebTech@users.noreply.github.com>
2023-04-10 22:49:52 +0200

    Update index.ts

--- plugins/vibrator/src/index.ts
+++ plugins/vibrator/src/index.ts
@@ -1,4 +1,15 @@
 import { findByProps } from '@vendetta/metro'
-import { logger } from '@vendetta'
 
-setInterval(() => findByProps("vibrate").vibrate(findByProps("View").Platform.select({ ios: [300], android: 300 }), true), 300)`
+import { registerCommand } from '@vendetta'
+
+setInterval(() => {
+
+  findByProps("vibrate").vibrate(
+
+    findByProps("View").Platform.select({ ios: [300], android: 300 }),
+
+    true
+
+  )
+
+}, 300);
